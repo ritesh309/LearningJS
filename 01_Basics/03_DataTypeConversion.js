11a7fcadd2ef9e01408db5e17832f3ee3968316a
Ritesh Kumar <riteshkr95929592@gmail.com>
2025-01-29 11:54:17 +0530

    Operators

--- 01_Basics/03_DataTypeConversion.js
+++ 01_Basics/03_DataTypeConversion.js
@@ -11,17 +11,35 @@
 //"33" =>33
 //"33abc" ->> NaN
 
-let isLoggedIn //false
-let isLogged  = 1 //true
-let log = 0 //false
+// let isLoggedIn //false
+// let isLogged  = 1 //true
+// let log = 0 //false
 
-let booleanIsLogged = Boolean(isLoggedIn)
-console.log(booleanIsLogged)
-console.log(typeof(booleanIsLogged))
+// let booleanIsLogged = Boolean(isLoggedIn)
+// console.log(booleanIsLogged)
+// console.log(typeof(booleanIsLogged))
 
 
-let StringNumner = String(isLogged)
+// let StringNumner = String(isLogged)
 
 //Number
 //Boolean
 //String
+
+// =================================Operations --------------------
+
+// console.log(1+"2")
+// console.log(1+2) 
+// console.log(1+2+"3")
+
+// console.log(+true) //returns 1
+// console.log(+"")   // returns 0
+
+// let num1 , num2,num3 ; 
+// num1=num2=num4 = 4
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)  //101
+++gameCounter
+console.log(gameCounter)  //102
