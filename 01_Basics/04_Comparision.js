53287b651feab78366ad3e5e10918e1b173706db
Ritesh Kumar <riteshkr95929592@gmail.com>
2025-01-29 12:03:56 +0530

    StackAndHeaps

---
+++ 01_Basics/04_Comparision.js
@@ -0,0 +1,27 @@
+// console.log(1>3)
+// console.log(4<=5)
+// console.log("2">5) //false
+// console.log("02">5) //false
+
+// let  p = null
+// console.log(typeof(p)) //object
+let p =null
+
+//null will never return a true value // its always 0
+
+// if(p){
+//     console.log("null")
+// }
+// else{
+//     console.log("not working ")
+// }
+
+// console.log(null==0) //false
+// console.log(undefined==0) //false
+
+//strict check === checks the data type as well 
+
+let a=3
+let b= "3"
+console.log(a==b)//true
+console.log(a===b)//false
