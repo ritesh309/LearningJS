1eec75cdc060745a09f98574e32b5e05871dd0bf
Ritesh Kumar <riteshkr95929592@gmail.com>
2025-01-29 13:17:58 +0530

    Dates

---
+++ 01_Basics/07_Dates.js
@@ -0,0 +1,9 @@
+// let myDate =new Date() //object
+// console.log(myDate.toString()) //Wed Jan 29 2025 13:09:12 GMT+0530 (India Standard Time)
+// console.log(myDate.toDateString())  //Wed Jan 29 2025
+
+// let myCalendar = new Date(2015,0,25)
+// console.log((myCalendar.toDateString()))  //Sun Jan 25 2015
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp)
