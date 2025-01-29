4c5e99b90b35ab9c3f7045f2ba1138aef846f791
Ritesh Kumar <riteshkr95929592@gmail.com>
2025-01-29 13:05:33 +0530

    MAths

--- 01_Basics/06_Strings.js
+++ 01_Basics/06_Strings.js
@@ -8,9 +8,16 @@
 // const newString1 = "   hiteshs f  "
 // console.log(newString1)
 // console.log(newString1.trim())  //removes start and end spaces only
-const url = "http://www.ritesh.com/help%20rhjs"
-// const newURL = url.replace("%20","-")
-// console.log(newURL)
+// const url = "http://www.ritesh.com/help%20rhjs"
+// // const newURL = url.replace("%20","-")
+// // console.log(newURL)
 
-const arr = url.split("/") //splits strings to array from a specific point
-console.log(arr)
+// const arr = url.split("/") //splits strings to array from a specific point
+// console.log(arr)
+
+// Math.round(4.56)
+// Math.ceil(4.23)
+// console.log(Math.sqrt(4))
+// console.log(Math.random())  //betwn 0-1 
+
+console.log((Math.random()*10)+1)
