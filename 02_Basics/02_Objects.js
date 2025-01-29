dc609d063ffbed2e3ad8b16a3f18490bad1aae28
Ritesh Kumar <riteshkr95929592@gmail.com>
2025-01-29 22:47:27 +0530

    Objects Iteration

--- 02_Basics/02_Objects.js
+++ 02_Basics/02_Objects.js
@@ -1,3 +1,5 @@
+
+//object literal -----------------
 //using symbol - Its for uniqueness
 const mySym = Symbol("sym")
 
@@ -18,13 +20,71 @@ const myObj={
 
 //decalare function'
 
-myObj.greet = function(){
-    console.log(this.name + "Good morning")
+// myObj.greet = function(){
+//     console.log(this.name + "Good morning")
 
-}
-console.log(myObj.greet()) 
-myObj.greet2 = function(){
-    console.log(this.name + " Good morning")
+// }
+// console.log(myObj.greet()) 
+// myObj.greet2 = function(){
+//     console.log(this.name + " Good morning")
 
-}
+// }
 // console.log(myObj.greet2())  //ritesh Good morning
+
+//-----------------------object singlton
+ const user = new Object()  // this is singlton object
+
+ user.id="123"
+ user.name="Riesh"
+ user.isLoggedIn = false
+
+//  console.log(user)
+
+//optional chaining
+const regUser ={
+    email:"ritesh@gmail.com",
+    fullName:{
+        firstName:"Ritesh",
+        lastName:"Kumar"
+    }
+
+}
+// console.log(regUser?.fullName?.firstName)  //?. it check the null values
+
+const obj1={
+    1:"a",
+    2:"b"
+}
+
+// const obj3 = {regUser , user}  //returbs object inside object
+
+// {
+//     regUser: {
+//       email: 'ritesh@gmail.com',
+//       fullName: { firstName: 'Ritesh', lastName: 'Kumar' }
+//     },
+//     user: { id: '123', name: 'Riesh', isLoggedIn: false }
+//   }
+
+const obj3 = Object.assign(user,regUser)
+const obj4 = Object.assign({},user,regUser) //using {} same returns guaranteed target and sourcs all the values goes to {} empty object
+// {
+//     id: '123',
+//     name: 'Riesh',
+//     isLoggedIn: false,
+//     email: 'ritesh@gmail.com',
+//     fullName: { firstName: 'Ritesh', lastName: 'Kumar' }
+//   }
+
+
+//spread operatior
+
+const obj5 = {...obj1,...myObj,...user}
+// console.log(obj5)
+
+
+// console.log(user)
+console.log(Object.keys(user))  //returns array
+console.log(Object.values(user))  //  returns array
+
+console.log(user.hasOwnProperty("nam"))  //returns true/false
