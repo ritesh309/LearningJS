10b3c2a27f6c0a7b4169179f2c057db2173feaf7
Ritesh Kumar <riteshkr95929592@gmail.com>
2025-01-29 12:51:04 +0530

    Strings

---
+++ 01_Basics/06_Strings.js
@@ -0,0 +1,16 @@
+// const game ="Cricket"
+// const gameName = new String("Hello")
+// gameName.charAt(2)
+// game.indexOf(3)
+// console.log(gameName.slice(-3,0))
+
+
+// const newString1 = "   hiteshs f  "
+// console.log(newString1)
+// console.log(newString1.trim())  //removes start and end spaces only
+const url = "http://www.ritesh.com/help%20rhjs"
+// const newURL = url.replace("%20","-")
+// console.log(newURL)
+
+const arr = url.split("/") //splits strings to array from a specific point
+console.log(arr)
