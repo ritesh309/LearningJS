7936bf3ce68c9a859efde112fc8013fbf37cf666
Ritesh Kumar <riteshkr95929592@gmail.com>
2025-01-30 13:20:31 +0530

    IIFE

---
+++ 03_Basics/04_IIFE.js
@@ -0,0 +1,41 @@
+ // Immediatly invoked funtion expression (IIFE)
+
+
+//  function hell(){
+//     console.log("This is hell")
+//  }
+//  hell()
+
+
+// directly runs the function
+
+//  (
+//     function helloji (){
+//         console.log("DB connected")
+//     }
+//  )() ; // we need to use semi colon to end this function weather it gives err
+
+//  //invoking arrow function
+//  (
+//       ()=>{
+//         console.log("DB ")
+//     }
+//  )();
+
+//  (
+//     (name)=>{
+//       console.log(name)
+//     }
+//  )()  // undefined
+
+(
+    (name )=>{
+      console.log(name )
+    }
+ )("Ritesh" );  // returns Ritesh
+
+ (
+    (...name )=>{
+      console.log(name )
+    }
+ )("Ritesh" , "Hell")  // returns the array [ 'Ritesh', 'Hell' ]
